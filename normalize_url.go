f00b6ef1d0e8ebfa5bc7be3b758b011d8fdb197c
itency <itency@outlook.com>
2024-09-08 14:36:39 +0300

    parse raw URL string into a URL struct

--- normalize_url.go
+++ normalize_url.go
@@ -1,5 +1,17 @@
 package main
 
-func normalizeURL(url string) (string, error) {
+import (
+	"net/url"
+	"strings"
+)
 
+func normalizeURL(urlStr string) (string, error) {
+	url, err := url.Parse(urlStr)
+	if err != nil {
+		return "", err
+	}
+
+	host := url.Hostname()
+	path := strings.TrimSuffix(url.Path, "/")
+	return host + path, nil
 }
